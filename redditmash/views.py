d6b68b45ecbd850009a7a57965df31d513df09c4
kalail <kashif610@gmail.com>
2012-11-10 04:21:20 -0600

    Add rankings view and url.

--- redditmash/views.py
+++ redditmash/views.py
@@ -6,6 +6,7 @@ from redditmash.models import Post, StatsReddit, Stats, Choice
 
 def index(request):
 	# POST request
+	# ------------------------------------------------
 	if request.is_ajax() and request.method == 'POST':
 		# Check if the choice is stale
 		try:
@@ -15,12 +16,16 @@ def index(request):
 		except Choice.DoesNotExist:
 			# Choice is stale - Do nothing
 			pass
+		# redirect to index for next choice
 		HttpResponseRedirect(reverse('redditmash.views.index'))
 
 	# GET request
+	# ------------------------------------------------
+	# Get or create choices sent session variable
 	if not request.session.get('choices_sent', None):
 		request.session['choices_sent'] = []
 
+	# Get ordered list of available choices
 	choices_active = Choice.objects.filter(is_active=True).order_by('-times_completed')
 	
 	# If there are no available choices render error page
@@ -40,3 +45,6 @@ def index(request):
 	request.session['choices_sent'].append(choice_to_use.id)
 	request.session.modified = True
 	return render_to_response('index.html', context_instance=RequestContext(request))
+
+def rankings(request):
+	return render_to_response('rankings.html', context_instance=RequestContext(request))
